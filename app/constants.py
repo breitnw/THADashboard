530f7d6e9c6d5f4a25f500aeac34ea6d79467ea4
Nick Breitling <Breitling.nw@gmail.com>
2022-12-22 16:02:24 -0600

    Rename hubs to reflect data in Onfleet

--- app/constants.py
+++ app/constants.py
@@ -2,4 +2,4 @@
 # TODO: should ideally be generated from within the app, then stored in the database
 # West: 7600 Victoria drive, Victoria MN 55386
 # East: 1835 North Penn Avenue, Minneapolis MN 55411
-HUB_LOCATION_COORDS = {"West": (44.867340, -93.674630), "East": (44.998190, -93.308270), "WIS": (44.956050,-93.135910)}
+HUB_LOCATION_COORDS = {"WEST": (44.867340, -93.674630), "NORTH POINT": (44.998190, -93.308270), "WIS": (44.956050,-93.135910)}
